8f8aa690fa0bd16e8a7befacb54271bd16be1fbc
Minstones <mumin96303@gmail.com>
2023-04-10 21:22:10 +0700

    Structure

---
+++ src/components/Global_components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Nav } from './Navbar.element';
+
+const Navbar = () => {
+    return(
+        <>
+            <Nav>Hello, World</Nav>
+        </>
+    )
+}
+export default Navbar ;
---
+++ src/pages/history.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/Global_components/Navbar";
+
+const History = () => {
+    return(
+        <>
+            <Navbar />
+        </>
+    )
+}
+export default History;
