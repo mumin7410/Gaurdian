31adaa43cfef6844647e7d05daaf96bdf0f4aa5d
sarif pochi <sarifpochi747@gmail.com>
2023-04-11 01:17:10 +0700

    c4

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <History />
-      <h1>hello world 1</h1>
+      <h1>hello world 1 hello worrld 1</h1>
       <h1>hello world 2</h1>
       <h1>hello world 3</h1>
       <h1>hello world 5</h1>
