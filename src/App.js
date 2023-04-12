039a1237ba1dfc538977bb08b48de83640847838
Minstones <mumin96303@gmail.com>
2023-04-12 21:44:01 +0700

    ExeptImage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import History from './pages/history';
 import Purpose from './components/History_components/Purpose_section';
 import Footer from './components/Global_components/Footer';
-
+import Standard_section from "./components/History_components/Standard_section";
+import Quote_section from "./components/History_components/Quote_section";
+import Navbar from "./components/Global_components/Navbar";
 function App() {
   return (
     <>
+      <Navbar />
       <History />
       <Purpose />
+      <Standard_section/>
+      <Quote_section/>
       <Footer />
     </>
   );
